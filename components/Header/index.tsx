305c2ae55af0aba9486f23f24b68fb75eff3d557
Radhika-1707 <radhikapullu17@gmail.com>
2023-06-09 20:22:46 +0530

    adds footer and fixes get in touch

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { SocialIcon } from 'react-social-icons'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
 type Props = {}
 
 
@@ -7,19 +8,19 @@ export default function Header({ }: Props) {
     return (
         <header className=' p-5 sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
             <motion.div className='flex flex-row items-center'
-            initial={{
-                x:-500,
-                opacity:0,
-                scale:0.5
-            }}
-            animate={{
-                x:0,
-                opacity:1,
-                scale:1
-            }}
-            transition={{
-                duration:1.5
-            }}
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5
+                }}
             >
                 <SocialIcon url="https://twitter.com/jaketrent"
                     fgColor='gray'
@@ -38,30 +39,34 @@ export default function Header({ }: Props) {
                     bgColor='transparent'
                 />
             </motion.div>
-            <motion.div className='flex flex-row items-center cursor-pointer text-gray-300'
-            initial={{
-                x:500,
-                opacity:0,
-                scale:0.5
-            }}
-            animate={{
-                x:0,
-                opacity:1,
-                scale:1
-            }}
-            transition={{
-                duration:1.5
+            <Link href='#contact'>
 
-            }}
-            >
-                <SocialIcon
-                    className='curor-pointer'
-                    network='email'
-                    fgColor='gray'
-                    bgColor='transparent'
-                />
-                <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
-            </motion.div>
+                <motion.div className='flex flex-row items-center cursor-pointer text-gray-300'
+                    initial={{
+                        x: 500,
+                        opacity: 0,
+                        scale: 0.5
+                    }}
+                    animate={{
+                        x: 0,
+                        opacity: 1,
+                        scale: 1
+                    }}
+                    transition={{
+                        duration: 1.5
+
+                    }}
+                >
+                    <SocialIcon
+                        className='curor-pointer'
+                        network='email'
+                        fgColor='gray'
+                        bgColor='transparent'
+                    />
+                    <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+                </motion.div>
+            </Link>
+
         </header>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Experience from '../components/Experience'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
 import Contact from '../components/Contact'
+import Link from 'next/link'
 export default function Home() {
   return (
 
@@ -37,6 +38,15 @@ export default function Home() {
       <section id='contact'className='snap-start' >
         <Contact/>
       </section>
+      <Link href="#hero">
+      <footer className='sticky bottom-5 w-full cursor-pointer'>
+        <div  className='flex items-center justify-center'>
+          <img
+          className='w-10 h-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
+          src='/user.png' alt=''/>
+        </div>
+      </footer>
+      </Link>
     </div>
   )
 }
